1b3917796b184b6fb47a2e0cd8f4ce9fa8c40aa9
DHJ99 <dananjayamadushanka825@gmail.com>
2025-05-07 10:55:48 +0530

    Usertable

--- src/Users.js
+++ src/Users.js
@@ -1,8 +1,31 @@
+import { Box } from "@mui/material";
 import UserForm from "./UserForm";
+import UserTable from "./UserTable";
+
+const users = [
+    {
+        id:1,
+        name: 'Amal'
+
+    },
+    {
+        id:2,
+        name: 'Bimal'
+
+    },
+    {
+        id:3,
+        name: 'Kamal'
+
+    }
+]
 
 const Users = () => {
    return (
-        <UserForm/>    
+     <Box>
+        <UserForm/>
+        <UserTable rows={users}/>
+     </Box>  
     );
 }
 
