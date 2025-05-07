b9da941036f7cbe013d926c5fa19d338913a3844
DHJ99 <dananjayamadushanka825@gmail.com>
2025-05-07 08:46:41 +0530

    user table

--- src/UserTable.js
+++ src/UserTable.js
@@ -1,8 +1,19 @@
-import {TableContainer } from '@mui/material'
+import {Paper, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 
 const UserTable = props =>{
-    <TableContainer>
-            
+    <TableContainer component={Paper}>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                
+            </TableBody>
+        </Table>
     </TableContainer>
 }
 
